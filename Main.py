1399fa6def7b136029b13b8a06e56e2d3919c2ec
tikhomirovd <dimasta00@gmail.com>
2018-01-24 00:20:05 +0300

    Написал много комментариев. Избавился от ненужны переменных

--- Main.py
+++ Main.py
@@ -1,13 +1,24 @@
-import random as r
+# step - Количество ходов
+# lvlupXP - количество опыта на первый уровень
+# Number_of_Players - Количество игроков
+# name - Имена игроков
+# Table_races - Таблица статов для разных рас
+# Table_name_file - Имя для сохранения таблицы статов
+# Status_name_file - Имя для сохранения таблицы статус-бара
+# Race_name - Названия рас
+# Stats_name -Названия статов
+# Bar_name - Названия статус-баров
+# Table - Таблица статов
+# Table_bar - Таблица статус-бара
+
+
+import random as r # Импортируем библиотеку рандома
 
 step = 0
+lvlupXP = 100
 Number_of_Players = int(input('Введите количество игроков: '))
-choises = ['1', '2', '3', '4']
 name = []
-Table_stats = {}
-Table_names = {}
 Table_races = []
-dict_Race_name = {}
 Table_name_file = 'Table1.txt'
 Status_name_file = 'Status1.txt'
 
@@ -34,7 +45,7 @@ def TableRaces():
     Table_races.append([1, 2, 1, 1, 2, 2])  # Нежить
     Table_races.append([1, 4, 3, 3, 3, 1])  # Демонстраторы
 
-
+# Выбор расы для каждого игрока в начале игры
 def choise_race(number : int):
     print('Выберете расу:')
     for i in range(len(Race_name)):
@@ -43,7 +54,7 @@ def choise_race(number : int):
     for i in range(len(Stats_name)):
         Table[number] = Table_races[name_race - 1]
 
-
+# Вывод таблицы статов
 def print_Table():
     print('%-20s' % '', end=' ')
     for i in range(Number_of_Players):
@@ -56,7 +67,7 @@ def print_Table():
             print('%-9s' % Table[j][i], end=' ')
         print()
 
-
+# Вывод таблицы статус-бара
 def print_Bar():
     print('%-20s' % '', end=' ')
     for i in range(Number_of_Players):
@@ -69,36 +80,45 @@ def print_Bar():
             print('%-9s' % Table_bar[j][i], end=' ')
         print()
 
-
+# Большой некрасивый вывод, который сделал отдельной функцией
 def menu():
     print('''
     1) Загрузить файл
     2) Вывести таблицу игроков
     3) Вывести статус-бар игроков
     4) Новый ход
-    5) 
+    5) Изменить характеристики игрока
     ''')
 
-
+# Шанс спас-броска (пока ещё не пригодился)
 def chance():
     L = int(input('Введите удачу '))
     chances = (L * 0.03) / (1 + L * 0.03)
     print('Шанс спас-броска равен ', '{:.2%}'.format(chances))
 
-
+# Выбор одного имени для действия
 def choise_name():
     for i in range(len(name)):
         print(i, ') ', name[i], sep='')
     a = int(input('Выберете игрока:'))
     return name[a]
 
+# Выбор несколькоих имён для действия
+def choise_name_AOE():
+    for i in range(len(name)):
+        print(i, ') ', name[i], sep='')
+    a = list(map(int, input('Выберете игроков:').split()))
+    return a
 
+# Поднятие уровня (ещё не доработана)
+# Будет сделано добавление статов игроку
+# И возможно что-то ещё
 def lvlUP(j : int):
     name_lvlUP = name[j]
     Table_bar[j][2] += 1
     print('Игрок ', name_lvlUP, 'получил новый уровень! ')
 
-
+# Сохраняем две таблицы в два отдельных файла
 def save_file(Table, Table_bar):
     f = open(Table_name_file, 'w')
     for i in range(Number_of_Players):
@@ -108,11 +128,14 @@ def save_file(Table, Table_bar):
 
     f = open(Status_name_file, 'w')
     for i in range(Number_of_Players):
+        # Преобразовываем числа в строки
+        # Люблю генераторы в Питоне
+        # Всё можно делать в одну строку с:
         a = ' '.join([str(i) for i in Table_bar[i]])
         f.write(a + '\n')
     f.close()
 
-
+# Загружаем таблицы
 def load_file():
     f = open(Table_name_file, 'r')
     a = f.readline()
@@ -132,10 +155,13 @@ def load_file():
         i += 1
         a = f.readline()
     f.close()
-    for i in range(len(Table_names)):
+    for i in range(len(name)):
         for j in range(Number_of_Players):
             Table[j][i] = float(Table[j][i])
 
+    # Это страшная кракозябра, которая преобразует все в числа
+    # Чтобы записать в файл, надо было все числа в строки преобразовать
+    # Тут как бы наоборот
     for i in range(len(Bar_name)):
         for j in range(Number_of_Players):
             Table_bar[j][i] = float(Table_bar[j][i])
@@ -143,39 +169,38 @@ def load_file():
     return Table, Table_bar
 
 
-TableRaces()
-
-
-def choise_name_AOE():
-    for i in range(len(name)):
-        print(i, ') ', name[i], sep='')
-    a = list(map(int, input('Выберете игроков:').split()))
-    return a
-
-
+# Подсчёт удара с крита
 def crit_damage(damage : float):
     cha = int(input('Введите шанс '))
     cha_damage = int(input('Введите увеличение урона '))
     rand = r.random()
 
+    # Тут возможно написан бред, но так работает
+    # А без бреда не оч
+    # Я про присваивания в else.
     if cha / 100 > 1 - rand:
         damage = damage * (1 + cha_damage / 100)
         return damage
     else:
-        return 0
+        damage = damage
+        return damage
 
+# Подсчёт других увеличений урона
 def another_damage(damage: float):
     cha_damage = int(input('Введите увеличение урона '))
     damage = damage * (1 + cha_damage / 100)
     return damage
 
-
+# Подсчёт сопротивления урона
 def resist_skill(damage: float):
     cha_damage = int(input('Введите сопротивление от скиллов '))
     damage = damage * (cha_damage / 100)
     return damage
 
-
+# Выбираем модификаторы урона
+# И с учётом вышеподсчитанного прибавляем\убавляем урон
+# Возвращает или саму себя (несколько критов и прочее)
+# Или урон, если отсутвует больше
 def resist_damage(damage: float):
     print('''
 1) Крит
@@ -208,7 +233,7 @@ def resist_damage(damage: float):
 
 
 
-
+# Подсчёт физического сопротивления в зависимости от характеристик
 def physics_resist(name_damage: int):
     agility = Table[name_damage][3]
     print(agility)
@@ -217,7 +242,7 @@ def physics_resist(name_damage: int):
     print('Сопротивление физическому урону равно', '{:.2%}'.format(PR))
     return PR
 
-
+# Подсчёт магического сопротивления в зависимости от характеристик
 def magic_resist(name_damage: int):
     wisdom = Table[name_damage][5]
     print(wisdom)
@@ -226,7 +251,7 @@ def magic_resist(name_damage: int):
     print('Сопротивление магическому урону равно', '{:.2%}'.format(MR))
     return MR
 
-
+# Выбор наносимого урона
 def choise_damage(damage: float, name_damage: int):
     print('''
 1) Физический
@@ -242,12 +267,16 @@ def choise_damage(damage: float, name_damage: int):
         damage = damage * (1 - mag)
         return damage
 
-
+# Выбираем кто получил урон
+# Считаем его, учитывая всё что угодно
+# Отнимаем жизни в таблице
 def in_damage(damage: float):
     name_damage = choise_name_AOE()
     kek = resist_damage(damage)
     damage = kek
     print(damage)
+    # Если одному человеку наносим, то не функция range()
+    # Не вызовется. Поэтому учитываем отдельно случай для одного человека
     if len(name_damage) == 1:
         choises = choise_damage(damage, name_damage[0])
         damage = choises
@@ -258,7 +287,7 @@ def in_damage(damage: float):
             Table_bar[name_damage[0]][0] = 0
             print('Игрок', name[name_damage[0]], 'Мёртв')
 
-
+    # Соответственно тут циклом многим людям отнимаем
     else:
         for i in range(len(name_damage)):
             choises = choise_damage(damage, name_damage[i])
@@ -270,7 +299,10 @@ def in_damage(damage: float):
                 Table_bar[name_damage[i]][0] = 0
                 print('Игрок', name[name_damage[i]], 'Мёртв')
 
-
+# Странное название функции
+# Но вроде всё работает
+# Так что зачем что-то менять?) :kappa:
+# Реген маны и жизней
 def test_regen(j: int):
     if Table_bar[j][0] + Table_bar[j][1] >= Table[j][1] * 20:
         Table_bar[j][0] = Table[j][1] * 20
@@ -282,7 +314,10 @@ def test_regen(j: int):
     else:
         Table_bar[j][2] += Table_bar[j][3]
 
-
+# Опять же много принтов
+# И сразу же выбор действия
+# И вызов различных функций, исходя из выбора
+# Пока ещё много чего зациклено, ибо не написаны функции
 def new_move():
     print('''
 1) Получение урона
@@ -310,44 +345,59 @@ def new_move():
     elif ch == '6':
         print_Bar()
         new_move()
+    else:
+        new_move()
 
 
-for i in range(len(Stats_name)):
-    Table_stats[Stats_name[i]] = i
+# Создаем таблицу рас
+TableRaces()
 
+# Вводим имена игроков и выбираем им расу
 for i in range(Number_of_Players):
     name.append(input('Введите имя игрока: '))
-    Table_names[name[i]] = i
     choise_race(i)
 
+# Создаем начальные запасы HP, MP и прочего
 for i in range(Number_of_Players):
     Table_bar[i][0] = 20 * Table[i][1]  # HP
     Table_bar[i][2] = 15 * Table[i][4]  # MP
-    Table_bar[i][1] = round(Table[i][1] / 15)
-    Table_bar[i][3] = round(Table[i][5] / 15)
+    Table_bar[i][1] = round(Table[i][1] / 15) # HP regen
+    Table_bar[i][3] = round(Table[i][5] / 15) # MP regen
     Table_bar[i][4] = 1  # LVL
     Table_bar[i][6] = 100  # Current XP
 
+# Зацикленная сама игра
 while True:
     menu()
 
     choise = input('Выберите требуемое действие:')
+    # Загрузить файл
     if choise == '1':
         Table, Table_bar = load_file()
 
+    # Вывести таблицу игроков
     elif choise == '2':
         print_Table()
 
-
+    # Вывести статус-бар игроков
     elif choise == '3':
         print_Bar()
 
+    # Новый ход
     elif choise == '4':
         new_move()
         step += 1
         print('Идёт', step, 'ход')
 
+    # Измена характеристик
+    elif choise == '5':
+        change_name = choise_name()
+        
+        new_move()
+
+    # Проверка на опыт
     for j in range(Number_of_Players):
-        if Table_bar[j][5] >= Table_bar[j][6] * (1.1 ** (Table_bar[j][4] - 1)):
+        Table_bar[j][6] = round(lvlupXP * (1.1 ** Table_bar[j][4] - 1))
+        if Table_bar[j][5] >= Table_bar[j][6]:
             lvlUP(j)
     save_file(Table, Table_bar)
